bc3a20150ae5971eb8bd1c21e83fbaa0ad248254
Oliver Coimbra <olivercoimbra@gmail.com>
2022-03-07 18:48:22 -0400

    Add teacher class

---
+++ teacher.rb
@@ -0,0 +1,12 @@
+require '/person'
+
+Class Teacher < Person
+  def initialize(specialization, age, name = 'Unknown', parent_permission: true)
+    @specialization = specialization
+  end
+
+  def can_use_services?
+    true
+  end
+
+end
